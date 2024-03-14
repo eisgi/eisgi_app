5a68f545ff80bd7b901b8519e3c70e2632b1635a
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-03-14 23:07:00 +0000

    update style

--- app/Imports/FormateurImport.php
+++ app/Imports/FormateurImport.php
@@ -16,24 +16,6 @@ class FormateurImport implements ToCollection
     public function collection(Collection $rows)
     {
         foreach ($rows as $row) {
-            // Assurez-vous que la ligne contient suffisamment de colonnes
-            if (count($row) < 6) {
-                continue; // Passez à la ligne suivante si une colonne "password" n'est pas présente
-            }
-
-            // Récupérez la valeur de la colonne "matricule" du fichier CSV
-            $matricule = $row[4];
-
-            // Formatez la date de naissance au format aaaa-mm-jj
-            $dateNaissance = \Carbon\Carbon::createFromFormat('d-m-Y', $row[2])->format('Y-m-d');
-
-            // Formatez la date de rejoindre au format aaaa-mm-jj
-            $dateRejoint = \Carbon\Carbon::createFromFormat('d-m-Y', $row[3])->format('Y-m-d');
-
-            // Cryptez le mot de passe
-            $password = Hash::make($row[5]);
-
-            // Créez l'enregistrement Formateur
             Formateur::create([
                 'nom' => $row[0],
                 'prenom' => $row[1],

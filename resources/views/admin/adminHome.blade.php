af91440842de2ab0eb67b41312d328d6529adcc6
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-03-15 22:50:33 +0000

    update admin home

--- resources/views/admin/adminHome.blade.php
+++ resources/views/admin/adminHome.blade.php
@@ -7,14 +7,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin home</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    
+
 </head>
 
 <body>
     <div class="container">
         <header>
-            <h1>Bonjour .{{$user->nom}}</h1>
+            <h1>Bonjour .{{ $user->nom }}</h1>
         </header>
+        <div class="alert alert-warning" role="alert">
+            Attention ! Vous êtes actuellement connecté en tant qu'administrateur principal. Soyez prudent avec les
+            actions que vous effectuez.
+        </div>
 
         <section id="ajout-forms">
             @include('frontend.forms.ajoutFCSV')
@@ -25,7 +29,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    
+
 </body>
 
 </html>
--- resources/views/frontend/components/formHead.blade.php
+++ resources/views/frontend/components/formHead.blade.php
@@ -1,13 +1,15 @@
 <div class="container mt-5">
-        <h1>Welcome, {{ $user->nom }}</h1>
-        <p>Your role: {{ $user->role }}</p>
-        
-        <div class="alert alert-primary" role="alert">
-            This is an example of an alert.
-        </div>
-       
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <button type="submit" class="btn btn-danger">Logout</button>
-        </form>
+    <h1>Welcome, {{ $user->nom }}</h1>
+    <p>Your role: {{ $user->role }}</p>
+
+    <div class="alert alert-info" role="alert">
+        Bienvenue, cher formateur ! Vous avez maintenant accès à toutes les fonctionnalités de l'interface
+        d'administration.
     </div>
+
+
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <button type="submit" class="btn btn-danger">Logout</button>
+    </form>
+</div>

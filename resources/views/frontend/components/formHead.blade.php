d7a939eb08dcc1de04103022e2902758f8c485e6
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-03-24 15:33:17 +0000

    fourmateur dasboard 1

--- resources/views/frontend/components/formHead.blade.php
+++ resources/views/frontend/components/formHead.blade.php
@@ -1,61 +1,61 @@
+<header>
+    <div class="logo" title="University Management System">
 
-    <header>
-        <div class="logo" title="University Management System">
-            <img src="./images/logo.png" alt="">
-            <h2>U<span class="danger">M</span>S</h2>
-        </div>
-        <div class="navbar">
-            <a href="index.html" class="active">
-                <span class="material-icons-sharp">home</span>
-                <h3>Home</h3>
-            </a>
-            <a href="timetable.html" onclick="timeTableAll()">
-                <span class="material-icons-sharp">today</span>
-                 <a href="{{ route('emploi.fourmateur') }}" class="btn btn-primary mt-3">Consulter l'emploi du temps</a>
+        <h2>I<span class="danger">S</span>GI</h2>
+    </div>
+    <div class="navbar">
+        <a href="index.html" class="active">
+            <span class="material-icons-sharp">home</span>
+            <h3>Home</h3>
+        </a>
+        <a href="{{ route('emploi.fourmateur') }}">
+            <span class="material-icons-sharp">today</span>
 
 
-            </a>
-            <a href="marks.html">
-                <span class="material-icons-sharp">grid_view</span>
-                <h3>Marks</h3>
-            </a>
-            <a href="profile.html">
-                <span class="material-icons-sharp">grid_view</span>
-                <h3>Profile</h3>
-            </a>
+        </a>
 
-            <a href="#">
-                <span class="material-icons-sharp" onclick="">
-                    <form method="POST" action="{{ route('logout') }}" class="mt-3">
-                        @csrf
-                        <button type="submit" class="btn btn-danger">Déconnexion</button>
-                    </form>
-                </span>
+        <a href="#">
+            <span class="material-icons-sharp">grid_view</span>
+            <h3>Profile</h3>
+        </a>
 
-            </a>
-        </div>
-        <div id="profile-btn">
-            <span class="material-icons-sharp">person</span>
-        </div>
-        <div class="theme-toggler">
-            <span class="material-icons-sharp active">light_mode</span>
-            <span class="material-icons-sharp">dark_mode</span>
-        </div>
+        <a href="#" onclick="document.getElementById('logoutForm').submit();">
+            <span class="material-icons-sharp">logout</span>
+            <h3>Logout</h3>
+        </a>
 
-    </header>
-    <div class="container">
-        <aside>
-            <div class="profile">
-                <div class="top">
-                    <div class="profile-photo">
-                        <img src="./images/profile-1.jpg" alt="">
-                    </div>
-                    <div class="info">
-                        <p>Bienvenue, {{ $user->nom }} </p>
-                        <small class="text-muted">12102030</small>
-                    </div>
+        <form style="display: none" id="logoutForm" method="POST" action="{{ route('logout') }}" class="mt-3">
+            @csrf
+            <button type="submit" class="btn btn-danger">Déconnexion</button>
+        </form>
+
+
+
+
+    </div>
+    <div id="profile-btn">
+        <span class="material-icons-sharp">person</span>
+    </div>
+    <div class="theme-toggler">
+        <span class="material-icons-sharp active">light_mode</span>
+        <span class="material-icons-sharp">dark_mode</span>
+    </div>
+
+</header>
+<div class="container">
+    <aside>
+        <div class="profile">
+            <div class="top">
+                <div class="profile-photo">
+                    <img src="./images/profile-1.jpg" alt="">
                 </div>
-                <div class="about">
+                <div class="info">
+                    <p>Bienvenue, {{ $user->nom }} </p>
+                    <small class="text-muted">{{ $user->matricule }}</small>
+                </div>
+            </div>
+            {{--
+                    <div class="about">
                     <h5>DOB</h5>
                     <p>29-Feb-2020</p>
                     <h5>Contact</h5>
@@ -64,80 +64,79 @@
                     <p>unknown@gmail.com</p>
                     <h5>Address</h5>
                     <p>Ghost town Road, New York, America</p>
-                </div>
-            </div>
-        </aside>
-
-        <main>
-            <div class="timetable" id="timetable">
-                <div>
-                    <span id="prevDay">&lt;</span>
-                    <h2>Today's Classes</h2>
-                    <span id="nextDay">&gt;</span>
-                </div>
-                <span class="closeBtn" onclick="timeTableAll()">X</span>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Time</th>
-                            <th>Room No.</th>
-                            <th>Subject</th>
-                            <th>Class</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody></tbody>
-                </table>
-            </div>
-        </main>
-
-        <div class="right">
-            <div class="announcements">
-                <h2>Announcements</h2>
-                <div class="updates">
-                    <div class="message">
-                        <p> <b>Academic</b> Summer training internship with Live Projects.</p>
-                        <small class="text-muted">2 Minutes Ago</small>
-                    </div>
-                    <div class="message">
-                        <p> <b>Co-curricular</b> Global internship oportunity by Student organization.</p>
-                        <small class="text-muted">10 Minutes Ago</small>
-                    </div>
-                    <div class="message">
-                        <p> <b>Examination</b> Instructions for Mid Term Examination.</p>
-                        <small class="text-muted">Yesterday</small>
-                    </div>
-                </div>
-            </div>
-
-            <div class="leaves">
-                <h2>Teachers on leave</h2>
-                <div class="teacher">
-                    <div class="profile-photo"><img src="./images/profile-2.jpeg" alt=""></div>
-                    <div class="info">
-                        <h3>The Professor</h3>
-                        <small class="text-muted">Full Day</small>
-                    </div>
-                </div>
-                <div class="teacher">
-                    <div class="profile-photo"><img src="./images/profile-3.jpg" alt=""></div>
-                    <div class="info">
-                        <h3>Lisa Manobal</h3>
-                        <small class="text-muted">Half Day</small>
-                    </div>
-                </div>
-                <div class="teacher">
-                    <div class="profile-photo"><img src="./images/profile-4.jpg" alt=""></div>
-                    <div class="info">
-                        <h3>Himanshu Jindal</h3>
-                        <small class="text-muted">Full Day</small>
-                    </div>
-                </div>
-            </div>
+                </div>  --}}
 
         </div>
+    </aside>
+
+    <main>
+        <div class="timetable" id="timetable">
+            <div>
+                <span id="prevDay">&lt;</span>
+                <h2>Avencement</h2>
+                <span id="nextDay">&gt;</span>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Time</th>
+                        <th>Room No.</th>
+                        <th>Subject</th>
+                        <th>Class</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </div>
+    </main>
+
+    <div class="right">
+        <div class="announcements">
+            <h2>Announcements</h2>
+            <div class="updates">
+                <div class="message">
+                    <p> <b>Academic</b> Summer training internship with Live Projects.</p>
+                    <small class="text-muted">2 Minutes Ago</small>
+                </div>
+                <div class="message">
+                    <p> <b>Co-curricular</b> Global internship oportunity by Student organization.</p>
+                    <small class="text-muted">10 Minutes Ago</small>
+                </div>
+                <div class="message">
+                    <p> <b>Examination</b> Instructions for Mid Term Examination.</p>
+                    <small class="text-muted">Yesterday</small>
+                </div>
+            </div>
+        </div>
+
+        <div class="leaves">
+            <h2>Teachers on leave</h2>
+            <div class="teacher">
+                <div class="profile-photo"><img src="./images/profile-2.jpeg" alt=""></div>
+                <div class="info">
+                    <h3>The Professor</h3>
+                    <small class="text-muted">Full Day</small>
+                </div>
+            </div>
+            <div class="teacher">
+                <div class="profile-photo"><img src="./images/profile-3.jpg" alt=""></div>
+                <div class="info">
+                    <h3>Lisa Manobal</h3>
+                    <small class="text-muted">Half Day</small>
+                </div>
+            </div>
+            <div class="teacher">
+                <div class="profile-photo"><img src="./images/profile-4.jpg" alt=""></div>
+                <div class="info">
+                    <h3>Himanshu Jindal</h3>
+                    <small class="text-muted">Full Day</small>
+                </div>
+            </div>
+        </div>
+
     </div>
+</div>
 
-    <script src="{{ asset('style/fourmateurDshboard/timeTable.js') }}"></script>
-    <script src="{{ asset('style/fourmateurDshboard/app.js') }}"></script>
-
+<script src="{{ asset('style/fourmateurDshboard/timeTable.js') }}"></script>
+<script src="{{ asset('style/fourmateurDshboard/app.js') }}"></script>

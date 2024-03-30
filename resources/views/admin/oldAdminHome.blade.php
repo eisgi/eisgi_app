ed48be95af30b2e12fe277d5f0282bf4f38cf71e
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-03-30 23:15:13 +0000

    admin dashboard

---
+++ resources/views/admin/oldAdminHome.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>Bonjour .{{ $user->nom }}</h1>
+        </header>
+        <div class="alert alert-warning" role="alert">
+            Attention ! Vous êtes actuellement connecté en tant qu'administrateur principal. Soyez prudent avec les
+            actions que vous effectuez.
+        </div>
+
+        <section id="ajout-forms">
+            @include('frontend.forms.ajoutFCSV')
+            @include('frontend.forms.ajoutFourCSV')
+        </section>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>

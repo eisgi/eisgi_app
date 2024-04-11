50b1b84ed6c6515b9ed98cee7bf98d30a7d6581d
youness <sycaruskun@gmail.com>
2024-04-11 18:03:44 +0000

    remplissement des colonnes de migration p1

--- database/migrations/2024_04_11_160051_create_complexes_table.php
+++ database/migrations/2024_04_11_160051_create_complexes_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('complexes', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('idComplexe');
+            $table->string('nomComplexe', 50);
         });
     }
 

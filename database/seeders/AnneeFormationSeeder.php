ad0ddd53ba2372b2d727216c86d1f728ce9491cf
Yacin ELANSAR <yacinelansardev@gmail.com>
2024-04-23 16:18:49 -0700

    Seeders:AnneeFormation,Semestre,Semaine,Jour(not yet)

--- database/seeders/AnneeFormationSeeder.php
+++ database/seeders/AnneeFormationSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\AnneeFormation;
 
 class AnneeFormationSeeder extends Seeder
 {
@@ -12,6 +13,21 @@ class AnneeFormationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $datas = [
+            [
+                "anneeFormation" => '2024-2025',
+                "dateDebutAnneeFormation" => '2024-09-05',
+                "dateFinAnneeFormation" => '2025-07-05'
+            ],
+            [
+                "anneeFormation" => '2025-2026',
+                "dateDebutAnneeFormation" => '2025-09-05',
+                "dateFinAnneeFormation" => '2026-01-23'
+            ]
+
+        ];
+        foreach($datas as $data){
+            AnneeFormation::create($data);
+        }
     }
 }
--- database/seeders/JourSeeder.php
+++ database/seeders/JourSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Jour;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,15 @@ class JourSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $datas=[
+            // [
+            //     "libelle"=>,
+            //     "is_feriee"=>,
+            //     ""
+            // ],
+        ];
+        foreach($datas as $data){
+            Jour::create($data);
+        }
     }
 }

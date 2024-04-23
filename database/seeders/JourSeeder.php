4383bc3c416bfcd0c869a9bd300f2b95fce12a01
Yacin ELANSAR <yacinelansardev@gmail.com>
2024-04-23 16:46:31 -0700

    Ajout Seeders :AnneeFormationSeeder,SemestreSeeder,SemaineSeeder,JourSeeder

--- database/seeders/JourSeeder.php
+++ database/seeders/JourSeeder.php
@@ -14,11 +14,33 @@ class JourSeeder extends Seeder
     public function run(): void
     {
         $datas=[
-            // [
-            //     "libelle"=>,
-            //     "is_feriee"=>,
-            //     ""
-            // ],
+            [
+                "libelle"=>"vacance 1",
+                "is_feriee"=>1,
+                "id_Semaine"=>1
+            ],
+            [
+                "libelle"=>"Ce jour n'est pas un jour férié",
+                "is_feriee"=>0,
+                "id_Semaine"=>2
+            ],
+            [
+                "libelle"=>"Ce jour n'est pas un jour férié",
+                "is_feriee"=>0,
+                "id_Semaine"=>2
+            ],
+            [
+                "libelle"=>"Ce jour n'est pas un jour férié",
+                "is_feriee"=>0,
+                "id_Semaine"=>2
+            ],
+            
+            [
+                "libelle"=>"vacance 2",
+                "is_feriee"=>1,
+                "id_Semaine"=>3
+            ],
+            
         ];
         foreach($datas as $data){
             Jour::create($data);

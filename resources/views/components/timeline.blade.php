a94e437fad6a934b324986477f17507afd553d35
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-04-29 08:59:47 +0100

    update style v2

--- resources/views/components/timeline.blade.php
+++ resources/views/components/timeline.blade.php
@@ -1,23 +1,34 @@
 <ul>
-                <li><span>8:30</span></li>
-                <li><span>9:00</span></li>
-                <li><span>9:30</span></li>
-                <li><span>10:00</span></li>
-                <li><span>10:30</span></li>
-                <li><span>11:00</span></li>
-                <li><span>11:30</span></li>
-                <li><span>12:00</span></li>
-                <li><span>12:30</span></li>
-                <li><span>13:00</span></li>
-                <li><span>13:30</span></li>
-                <li><span>14:00</span></li>
-                <li><span>14:30</span></li>
-                <li><span>15:00</span></li>
-                <li><span>15:30</span></li>
-                <li><span>16:00</span></li>
-                <li><span>16:30</span></li>
-                <li><span>17:00</span></li>
-                <li><span>17:30</span></li>
-                <li><span>18:00</span></li>
-                <li><span>18:30</span></li>
-            </ul>
+    <li><span>8:30</span></li>
+    <li><span>9:00</span></li>
+    <li><span>9:30</span></li>
+    <li><span>10:00</span></li>
+    <li><span>10:30</span></li>
+    <li><span>11:00</span></li>
+    <li><span>11:30</span></li>
+    <li><span>12:00</span></li>
+    <li><span>12:30</span></li>
+    <li><span>13:00</span></li>
+    <li><span>13:30</span></li>
+    <li><span>14:00</span></li>
+    <li><span>14:30</span></li>
+    <li><span>15:00</span></li>
+    <li><span>15:30</span></li>
+    <li><span>16:00</span></li>
+    <li><span>16:30</span></li>
+    <li><span>17:00</span></li>
+    <li><span>17:30</span></li>
+    <li><span>18:00</span></li>
+    <li><span>18:30</span></li>
+</ul>
+
+
+{{--
+<ul>
+    @for ($i = 8; $i < 19; $i++)
+        @for ($j = 0; $j < 2; $j++)
+            <li><span>{{ str_pad($i, 2, '0', STR_PAD_LEFT) }}:{{ $j * 30 }}</span></li>
+        @endfor
+    @endfor
+</ul>
+--}}
